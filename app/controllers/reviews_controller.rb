b80b259c5ad749a977c9c5913f9d61a940e0509d
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-29 18:46:56 -0400

    destroy method for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,8 +7,22 @@ class ReviewsController < ApplicationController
 
 
     def create 
-        byebug
-        render json: 
+        @review = Review.create(review_params)
+        render json: @review   
+    end
+
+
+
+    def destroy
+        @review = Review.find_by(id: params[:id])
+        @review.destroy
+        render json: @review
+    end
+
+    private
+
+    def review_params
+        params.permit(:user_id, :flavor_id, :topping_id, :milk_id, :scoop_id, :review)
     end
 
 end
