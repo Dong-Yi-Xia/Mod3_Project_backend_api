3cbb5647c9adbe3e6a79c0ac607cca9d9973b9ed
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-10-27 21:33:05 -0400

    init

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2020_09_27_185920) do
     t.string "name"
     t.integer "price"
     t.string "image"
-    t.boolean "like"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
